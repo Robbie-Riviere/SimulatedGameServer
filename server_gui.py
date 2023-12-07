e08ca3fa051900041db552a1746be71645486187
Gabe Megna <47236825+gnm1714@users.noreply.github.com>
2023-12-07 17:35:50 -0500

    Skeleton for server GUI

---
+++ server_gui.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+
+def main():
+    window = tk.Tk()
+    window.title("Server")
+    window.geometry("500x500")
+
+    text_box = tk.Text(window, width=62, height=30)
+    users = [tk.Button(window, text=f"Player {i} Connected...\n", width=50, height=7) for i in range(1, 5)]
+
+    for i in range(len(users)):
+        text_box.insert(tk.END, users[i]['text'])
+
+    text_box.place(x=0, y=0)
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
